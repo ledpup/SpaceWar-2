48c34ca4d85b0c6721be82a6e597af9ab4aeb769
Patrick Lea <ledpup@gmail.com>
2018-04-25 19:33:32 +1000

    First go at adding a bullet emitter (not working)

---
+++ Assets/BulletBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public GameObject Bullet_Emitter;
+    public GameObject Bullet;
+    public float Bullet_Forward_Force;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var temporaryBulletHandler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+            //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+            //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
+            temporaryBulletHandler.transform.Rotate(Vector3.left * 90);
+
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = temporaryBulletHandler.GetComponent<Rigidbody>();
+
+            Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force * Time.deltaTime);
+
+            Destroy(temporaryBulletHandler, 3.0f);
+        }
+    }
+}
