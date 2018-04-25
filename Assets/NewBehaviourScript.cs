38b8c12742d9c8979292732dc29509ecb20ae228
Patrick Lea <ledpup@gmail.com>
2018-04-25 14:43:41 +1000

    Revert "Revert "Initial commit""
    This reverts commit 5174f2eb4feb915e0f1b40e9b5489d2876adccd1.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    // Use this for initialization
+    float heading;
+    public new Rigidbody rigidbody;
+    public Text Speed;
+    public Text Heading;
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate() {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var speed = (vertical > 0 ? vertical : vertical * .8f) * 20f;
+
+        var sign = Math.Sign(speed);
+        sign = sign == 0 ? 1 : sign;
+
+        heading += sign * (horizontal * 5) * Time.deltaTime;
+
+        rigidbody.AddRelativeForce(Vector3.up * speed);
+        transform.eulerAngles = new Vector3(0, 0, RadianToDegree(-heading));
+
+        Speed.text = "Speed " + Math.Round(rigidbody.velocity.magnitude, 1);
+        Heading.text = "Heading " + Math.Round(360 - transform.eulerAngles.z, 0);
+    }
+
+    private double DegreeToRadian(double angle)
+    {
+        return Math.PI * angle / 180.0;
+    }
+
+    private float RadianToDegree(float angle)
+    {
+        return (float)(angle * (180.0 / Math.PI));
+    }
+}
