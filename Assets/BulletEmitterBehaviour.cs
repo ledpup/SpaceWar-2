d2679369dbfbe46ee3d3fdde147d6068177b9c7f
Patrick Lea <ledpup@gmail.com>
2018-04-26 21:57:53 +1000

    Corrected bullet emitter

---
+++ Assets/BulletEmitterBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEmitterBehaviour : MonoBehaviour
+{
+    public GameObject BulletEmitter;
+    public GameObject Bullet;
+    public float BulletForce;
+    public float FiringRate;
+    float nextFire;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + FiringRate;
+
+            var bullet = Instantiate(Bullet, BulletEmitter.transform.position, BulletEmitter.transform.rotation) as GameObject;
+
+            var bulletRigidBody = bullet.GetComponent<Rigidbody>();
+            var shipRigidBody = GetComponent<Rigidbody>();
+
+            bulletRigidBody.velocity = shipRigidBody.velocity;
+            bulletRigidBody.AddForce(transform.up * BulletForce);
+
+            Destroy(bullet, 3.0f);
+        }
+    }
+}
--- Assets/PlayerShipBehaviour.cs
+++ Assets/PlayerShipBehaviour.cs
@@ -4,32 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerShipBehaviour : MonoBehaviour {
 
     // Use this for initialization
     float heading;
-    public new Rigidbody rigidbody;
+    Rigidbody _rigidbody;
     public Text Speed;
     public Text Heading;
     void Start () {
-        rigidbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 	
 	void FixedUpdate() {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
 
-        var speed = (vertical > 0 ? vertical : vertical * .8f) * 20f;
+        var speed = (vertical > 0 ? vertical : vertical * .6f) * 20f;
 
-        var sign = Math.Sign(speed);
-        sign = sign == 0 ? 1 : sign;
+        var sign = speed >= 0 ? 1 : -1;
 
         heading += sign * (horizontal * 5) * Time.deltaTime;
 
-        rigidbody.AddRelativeForce(Vector3.up * speed);
+        _rigidbody.AddRelativeForce(Vector3.up * speed);
         transform.eulerAngles = new Vector3(0, 0, RadianToDegree(-heading));
 
-        Speed.text = "Speed " + Math.Round(rigidbody.velocity.magnitude, 1);
+        Speed.text = "Speed " + Math.Round(_rigidbody.velocity.magnitude, 1);
         Heading.text = "Heading " + Math.Round(360 - transform.eulerAngles.z, 0);
     }
 
