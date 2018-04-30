0f7dafdc402b0f39bccc5f763deda9977e406a0d
Patrick Lea <ledpup@gmail.com>
2018-04-30 22:00:41 +1000

    Camera centering on mid-point between two objects
    Moved .unity file too

--- Assets/CameraBehaviour.cs
+++ Assets/CameraBehaviour.cs
@@ -6,6 +6,7 @@ public class CameraBehaviour : MonoBehaviour {
 
     double mapX, mapY = 100.0f;
     double minX, maxX, minY, maxY;
+    public GameObject t1, t2;
 
 	// Use this for initialization
 	void Start () {
@@ -17,10 +18,42 @@ public class CameraBehaviour : MonoBehaviour {
         maxX = mapX / 2.0 - horzExtent;
         minY = vertExtent - mapY / 2.0;
         maxY = mapY / 2.0 - vertExtent;
+
+
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void FixedCameraFollowSmooth(Camera cam, Transform t1, Transform t2)
+    {
+        // How many units should we keep from the players
+        float zoomFactor = 1.5f;
+        float followTimeDelta = 0.8f;
+
+        // Midpoint we're after
+        Vector3 midpoint = (t1.position + t2.position) / 2f;
+
+        // Distance between objects
+        float distance = (t1.position - t2.position).magnitude;
+
+        // Move camera a certain distance
+        Vector3 cameraDestination = midpoint - cam.transform.forward * distance * zoomFactor;
+
+        // Adjust ortho size if we're using one of those
+        if (cam.orthographic)
+        {
+            // The camera's forward vector is irrelevant, only this size will matter
+            cam.orthographicSize = distance;
+        }
+        // You specified to use MoveTowards instead of Slerp
+        cam.transform.position = Vector3.Slerp(cam.transform.position, cameraDestination, followTimeDelta);
+
+        // Snap when close enough to prevent annoying slerp behavior
+        if ((cameraDestination - cam.transform.position).magnitude <= 0.05f)
+            cam.transform.position = cameraDestination;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        FixedCameraFollowSmooth(GetComponent<Camera>(), t1.transform, t2.transform);
+    }
 }
